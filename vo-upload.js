4dd3ea1f55d200ecef8c1de5b71abf246d20d943
Tom Coemans <tom.coemans@gmail.com>
2018-11-28 11:58:05 +0100

    GVR-50 vo-upload demo publicatie

--- vo-upload.js
+++ vo-upload.js
@@ -11,6 +11,8 @@ import './node_modules/dropzone/dist/dropzone.js';
  *
  * Event | Uitleg | Detail object
  * ------|-------------|--------------
+ * `vo-upload-bestand-toegevoegd` | Wordt afgevuurd wanneer er een bestand werd toegevoegd. | { bestand }
+ * `vo-upload-bestand-verwijderd` | Wordt afgevuurd wanneer er een bestand werd verwijderd. | { bestand }
  * `vo-upload-versturen` | Wordt afgevuurd vlak voordat het bestand verstuurd wordt naar de url. | { bestand }
  * `vo-upload-succes` | Wordt afgevuurd wanneer een bestand succesvol werd opgeladen. | { bestand, response }
  * `vo-upload-fout` | Wordt afgevuurd wanneer een bestand niet kon opgeladen worden vanwege een fout. | { bestand, fout }
@@ -27,6 +29,9 @@ class VoUpload extends LitElement {
         super();
         Dropzone.autoDiscover = false;
         Dropzone.prototype.defaultOptions.dictDefaultMessage = 'Sleep bestanden naar hier om op te laden';
+        Dropzone.prototype.defaultOptions.dictCancelUpload = 'annuleer oplading';
+        Dropzone.prototype.defaultOptions.dictCancelUploadConfirmation = 'Ben je zeker dat je deze oplading wilt annuleren?';
+        Dropzone.prototype.defaultOptions.dictRemoveFile = 'verwijder bestand';
         Dropzone.prototype.defaultOptions.dictInvalidFileType = 'ONGELDIG_BESTANDSTYPE';
         Dropzone.prototype.defaultOptions.dictFileTooBig = 'BESTAND_TE_GROOT';
         Dropzone.prototype.defaultOptions.dictResponseError = 'HTTP_FOUT_{{statusCode}}';
@@ -34,43 +39,83 @@ class VoUpload extends LitElement {
         this.param = this.param || 'file';
     }
 
+    /**
+     * Geeft al de properties.
+     * 
+     * @return {Object}
+     */
     static get properties() {
         return {
             /**
              * URL waarnaar geupload moet worden.
              */
-            url: String,
+            url: {
+            	type: String
+            },
             /**
              * Een lijst van toegelaten extensies.
              */
-            'toegelaten-extensies': Array,
+            'toegelaten-extensies': {
+            	type: Array
+            },
             /**
              * Een lijst van toegelaten mimetypes.
              */
-            'toegelaten-mimetypes': Array,
+            'toegelaten-mimetypes': {
+            	type: Array
+            },
             /**
              * De maximum grootte van een bestand in bytes.
              */
-            'maximum-grootte': Number,
+            'maximum-grootte': {
+            	type: Number
+            },
             /**
              * Het veld in de multipart/form-data body waarin het bestand gestoken moet worden.
              */
-            param: String
+            param: {
+            	type: String
+            },
+            /**
+             * Het maximum aantal bestanden dat men mag uploaden.
+             */
+            'maximum-aantal-bestanden': {
+            	type: Number
+            },
+            /**
+             * Bepaalt of er automatisch geupload moet worden.
+             */
+            'auto-upload': {
+            	type: Boolean
+            }
         };
     }
 
     /**
      * Configuratie van de dropzone na de eerste rendering.
+     * 
+     * @Return {void}
      */
     firstUpdated() {
         const self = this;
         const element = this.shadowRoot.querySelector('#upload');
-        this.dropzone = new Dropzone(element, {
+        this._dropzone = new Dropzone(element, {
             url: this.url,
             paramName: this.param,
             maxFilesize: this._dropzoneMaxFilesize(),
             acceptedFiles: this._dropzoneAcceptedFiles(),
+            addRemoveLinks: true,
+            maxFiles: this['maximum-aantal-bestanden'],
+            autoProcessQueue: this['auto-upload'],
             init: function () {
+                this.on('addedfile', function(bestand) {
+                    self._fireEvent('bestand-toegevoegd', { bestand: VoUpload._dropzoneBestandNaarVoUploadBestand(bestand) });
+                });
+
+                this.on('removedfile', function(bestand) {
+                    self._fireEvent('bestand-verwijderd', { bestand: VoUpload._dropzoneBestandNaarVoUploadBestand(bestand) });
+                });
+
                 this.on('sending', function(bestand) {
                     self._fireEvent('versturen', { bestand: VoUpload._dropzoneBestandNaarVoUploadBestand(bestand) });
                 });
@@ -86,10 +131,20 @@ class VoUpload extends LitElement {
         });
     }
 
+    /**
+     * Berekent de maximum bestand grootte.
+     * 
+     * @return {Number}
+     */
     _dropzoneMaxFilesize() {
         return this['maximum-grootte'] / 1024 / 1024
     }
 
+    /**
+     * Genereert een array die de toegelaten bestand types bepaalt.
+     * 
+     * @return {Array}
+     */
     _dropzoneAcceptedFiles() {
         if (this['toegelaten-extensies'] || this['toegelaten-mimetypes']) {
             return [].concat(this['toegelaten-extensies']).concat(this['toegelaten-mimetypes']).filter((item) => item != null).join(',');
@@ -98,17 +153,32 @@ class VoUpload extends LitElement {
         }
     }
 
+    /**
+     * Gooit een vo-upload event.
+     * 
+     * @return {void}
+     */
     _fireEvent(naam, data) {
         this.dispatchEvent(new CustomEvent('vo-upload-' + naam, {detail: data}));
     }
 
     static _dropzoneBestandNaarVoUploadBestand(bestand) {
         return {
+            id: bestand.upload.uuid,
             naam: bestand.name,
             grootte: bestand.size,
             type: bestand.type,
             laatstGewijzigd: bestand.lastModified
         }
+    }
+
+    /**
+     * Upload alle bestanden die de gebruiker geselecteerd heeft.
+     * 
+     * @return {void}
+     */
+    upload() {
+        this._dropzone.processQueue();
     }
 
     /**
@@ -118,19 +188,45 @@ class VoUpload extends LitElement {
      */
     render() {
         return html`
-    		<link rel="stylesheet" type="text/css" href="../node_modules/dropzone/dist/min/basic.min.css">
-    		<link rel="stylesheet" type="text/css" href="../node_modules/dropzone/dist/min/dropzone.min.css">
     		<style>
+    			.dropzone,.dropzone *{box-sizing:border-box}.dropzone{position:relative}.dropzone .dz-preview{position:relative;display:inline-block;width:120px;margin:0.5em}.dropzone .dz-preview .dz-progress{display:block;height:15px;border:1px solid #aaa}.dropzone .dz-preview .dz-progress .dz-upload{display:block;height:100%;width:0;background:green}.dropzone .dz-preview .dz-error-message{color:red;display:none}.dropzone .dz-preview.dz-error .dz-error-message,.dropzone .dz-preview.dz-error .dz-error-mark{display:block}.dropzone .dz-preview.dz-success .dz-success-mark{display:block}.dropzone .dz-preview .dz-error-mark,.dropzone .dz-preview .dz-success-mark{position:absolute;display:none;left:30px;top:30px;width:54px;height:58px;left:50%;margin-left:-27px}
+        		@-webkit-keyframes passing-through{0%{opacity:0;-webkit-transform:translateY(40px);-moz-transform:translateY(40px);-ms-transform:translateY(40px);-o-transform:translateY(40px);transform:translateY(40px)}30%, 70%{opacity:1;-webkit-transform:translateY(0px);-moz-transform:translateY(0px);-ms-transform:translateY(0px);-o-transform:translateY(0px);transform:translateY(0px)}100%{opacity:0;-webkit-transform:translateY(-40px);-moz-transform:translateY(-40px);-ms-transform:translateY(-40px);-o-transform:translateY(-40px);transform:translateY(-40px)}}@-moz-keyframes passing-through{0%{opacity:0;-webkit-transform:translateY(40px);-moz-transform:translateY(40px);-ms-transform:translateY(40px);-o-transform:translateY(40px);transform:translateY(40px)}30%, 70%{opacity:1;-webkit-transform:translateY(0px);-moz-transform:translateY(0px);-ms-transform:translateY(0px);-o-transform:translateY(0px);transform:translateY(0px)}100%{opacity:0;-webkit-transform:translateY(-40px);-moz-transform:translateY(-40px);-ms-transform:translateY(-40px);-o-transform:translateY(-40px);transform:translateY(-40px)}}@keyframes passing-through{0%{opacity:0;-webkit-transform:translateY(40px);-moz-transform:translateY(40px);-ms-transform:translateY(40px);-o-transform:translateY(40px);transform:translateY(40px)}30%, 70%{opacity:1;-webkit-transform:translateY(0px);-moz-transform:translateY(0px);-ms-transform:translateY(0px);-o-transform:translateY(0px);transform:translateY(0px)}100%{opacity:0;-webkit-transform:translateY(-40px);-moz-transform:translateY(-40px);-ms-transform:translateY(-40px);-o-transform:translateY(-40px);transform:translateY(-40px)}}@-webkit-keyframes slide-in{0%{opacity:0;-webkit-transform:translateY(40px);-moz-transform:translateY(40px);-ms-transform:translateY(40px);-o-transform:translateY(40px);transform:translateY(40px)}30%{opacity:1;-webkit-transform:translateY(0px);-moz-transform:translateY(0px);-ms-transform:translateY(0px);-o-transform:translateY(0px);transform:translateY(0px)}}@-moz-keyframes slide-in{0%{opacity:0;-webkit-transform:translateY(40px);-moz-transform:translateY(40px);-ms-transform:translateY(40px);-o-transform:translateY(40px);transform:translateY(40px)}30%{opacity:1;-webkit-transform:translateY(0px);-moz-transform:translateY(0px);-ms-transform:translateY(0px);-o-transform:translateY(0px);transform:translateY(0px)}}@keyframes slide-in{0%{opacity:0;-webkit-transform:translateY(40px);-moz-transform:translateY(40px);-ms-transform:translateY(40px);-o-transform:translateY(40px);transform:translateY(40px)}30%{opacity:1;-webkit-transform:translateY(0px);-moz-transform:translateY(0px);-ms-transform:translateY(0px);-o-transform:translateY(0px);transform:translateY(0px)}}@-webkit-keyframes pulse{0%{-webkit-transform:scale(1);-moz-transform:scale(1);-ms-transform:scale(1);-o-transform:scale(1);transform:scale(1)}10%{-webkit-transform:scale(1.1);-moz-transform:scale(1.1);-ms-transform:scale(1.1);-o-transform:scale(1.1);transform:scale(1.1)}20%{-webkit-transform:scale(1);-moz-transform:scale(1);-ms-transform:scale(1);-o-transform:scale(1);transform:scale(1)}}@-moz-keyframes pulse{0%{-webkit-transform:scale(1);-moz-transform:scale(1);-ms-transform:scale(1);-o-transform:scale(1);transform:scale(1)}10%{-webkit-transform:scale(1.1);-moz-transform:scale(1.1);-ms-transform:scale(1.1);-o-transform:scale(1.1);transform:scale(1.1)}20%{-webkit-transform:scale(1);-moz-transform:scale(1);-ms-transform:scale(1);-o-transform:scale(1);transform:scale(1)}}@keyframes pulse{0%{-webkit-transform:scale(1);-moz-transform:scale(1);-ms-transform:scale(1);-o-transform:scale(1);transform:scale(1)}10%{-webkit-transform:scale(1.1);-moz-transform:scale(1.1);-ms-transform:scale(1.1);-o-transform:scale(1.1);transform:scale(1.1)}20%{-webkit-transform:scale(1);-moz-transform:scale(1);-ms-transform:scale(1);-o-transform:scale(1);transform:scale(1)}}.dropzone,.dropzone *{box-sizing:border-box}.dropzone{min-height:150px;border:2px solid rgba(0,0,0,0.3);background:white;padding:20px 20px}.dropzone.dz-clickable{cursor:pointer}.dropzone.dz-clickable *{cursor:default}.dropzone.dz-clickable .dz-message,.dropzone.dz-clickable .dz-message *{cursor:pointer}.dropzone.dz-started .dz-message{display:none}.dropzone.dz-drag-hover{border-style:solid}.dropzone.dz-drag-hover .dz-message{opacity:0.5}.dropzone .dz-message{text-align:center;margin:2em 0}.dropzone .dz-preview{position:relative;display:inline-block;vertical-align:top;margin:16px;min-height:100px}.dropzone .dz-preview:hover{z-index:1000}.dropzone .dz-preview:hover .dz-details{opacity:1}.dropzone .dz-preview.dz-file-preview .dz-image{border-radius:20px;background:#999;background:linear-gradient(to bottom, #eee, #ddd)}.dropzone .dz-preview.dz-file-preview .dz-details{opacity:1}.dropzone .dz-preview.dz-image-preview{background:white}.dropzone .dz-preview.dz-image-preview .dz-details{-webkit-transition:opacity 0.2s linear;-moz-transition:opacity 0.2s linear;-ms-transition:opacity 0.2s linear;-o-transition:opacity 0.2s linear;transition:opacity 0.2s linear}.dropzone .dz-preview .dz-remove{font-size:14px;text-align:center;display:block;cursor:pointer;border:none}.dropzone .dz-preview .dz-remove:hover{text-decoration:underline}.dropzone .dz-preview:hover .dz-details{opacity:1}.dropzone .dz-preview .dz-details{z-index:20;position:absolute;top:0;left:0;opacity:0;font-size:13px;min-width:100%;max-width:100%;padding:2em 1em;text-align:center;color:rgba(0,0,0,0.9);line-height:150%}.dropzone .dz-preview .dz-details .dz-size{margin-bottom:1em;font-size:16px}.dropzone .dz-preview .dz-details .dz-filename{white-space:nowrap}.dropzone .dz-preview .dz-details .dz-filename:hover span{border:1px solid rgba(200,200,200,0.8);background-color:rgba(255,255,255,0.8)}.dropzone .dz-preview .dz-details .dz-filename:not(:hover){overflow:hidden;text-overflow:ellipsis}.dropzone .dz-preview .dz-details .dz-filename:not(:hover) span{border:1px solid transparent}.dropzone .dz-preview .dz-details .dz-filename span,.dropzone .dz-preview .dz-details .dz-size span{background-color:rgba(255,255,255,0.4);padding:0 0.4em;border-radius:3px}.dropzone .dz-preview:hover .dz-image img{-webkit-transform:scale(1.05, 1.05);-moz-transform:scale(1.05, 1.05);-ms-transform:scale(1.05, 1.05);-o-transform:scale(1.05, 1.05);transform:scale(1.05, 1.05);-webkit-filter:blur(8px);filter:blur(8px)}.dropzone .dz-preview .dz-image{border-radius:20px;overflow:hidden;width:120px;height:120px;position:relative;display:block;z-index:10}.dropzone .dz-preview .dz-image img{display:block}.dropzone .dz-preview.dz-success .dz-success-mark{-webkit-animation:passing-through 3s cubic-bezier(0.77, 0, 0.175, 1);-moz-animation:passing-through 3s cubic-bezier(0.77, 0, 0.175, 1);-ms-animation:passing-through 3s cubic-bezier(0.77, 0, 0.175, 1);-o-animation:passing-through 3s cubic-bezier(0.77, 0, 0.175, 1);animation:passing-through 3s cubic-bezier(0.77, 0, 0.175, 1)}.dropzone .dz-preview.dz-error .dz-error-mark{opacity:1;-webkit-animation:slide-in 3s cubic-bezier(0.77, 0, 0.175, 1);-moz-animation:slide-in 3s cubic-bezier(0.77, 0, 0.175, 1);-ms-animation:slide-in 3s cubic-bezier(0.77, 0, 0.175, 1);-o-animation:slide-in 3s cubic-bezier(0.77, 0, 0.175, 1);animation:slide-in 3s cubic-bezier(0.77, 0, 0.175, 1)}.dropzone .dz-preview .dz-success-mark,.dropzone .dz-preview .dz-error-mark{pointer-events:none;opacity:0;z-index:500;position:absolute;display:block;top:50%;left:50%;margin-left:-27px;margin-top:-27px}.dropzone .dz-preview .dz-success-mark svg,.dropzone .dz-preview .dz-error-mark svg{display:block;width:54px;height:54px}.dropzone .dz-preview.dz-processing .dz-progress{opacity:1;-webkit-transition:all 0.2s linear;-moz-transition:all 0.2s linear;-ms-transition:all 0.2s linear;-o-transition:all 0.2s linear;transition:all 0.2s linear}.dropzone .dz-preview.dz-complete .dz-progress{opacity:0;-webkit-transition:opacity 0.4s ease-in;-moz-transition:opacity 0.4s ease-in;-ms-transition:opacity 0.4s ease-in;-o-transition:opacity 0.4s ease-in;transition:opacity 0.4s ease-in}.dropzone .dz-preview:not(.dz-processing) .dz-progress{-webkit-animation:pulse 6s ease infinite;-moz-animation:pulse 6s ease infinite;-ms-animation:pulse 6s ease infinite;-o-animation:pulse 6s ease infinite;animation:pulse 6s ease infinite}.dropzone .dz-preview .dz-progress{opacity:1;z-index:1000;pointer-events:none;position:absolute;height:16px;left:50%;top:50%;margin-top:-8px;width:80px;margin-left:-40px;background:rgba(255,255,255,0.9);-webkit-transform:scale(1);border-radius:8px;overflow:hidden}.dropzone .dz-preview .dz-progress .dz-upload{background:#333;background:linear-gradient(to bottom, #666, #444);position:absolute;top:0;left:0;bottom:0;width:0;-webkit-transition:width 300ms ease-in-out;-moz-transition:width 300ms ease-in-out;-ms-transition:width 300ms ease-in-out;-o-transition:width 300ms ease-in-out;transition:width 300ms ease-in-out}.dropzone .dz-preview.dz-error .dz-error-message{display:block}.dropzone .dz-preview.dz-error:hover .dz-error-message{opacity:1;pointer-events:auto}.dropzone .dz-preview .dz-error-message{pointer-events:none;z-index:1000;position:absolute;display:block;display:none;opacity:0;-webkit-transition:opacity 0.3s ease;-moz-transition:opacity 0.3s ease;-ms-transition:opacity 0.3s ease;-o-transition:opacity 0.3s ease;transition:opacity 0.3s ease;border-radius:8px;font-size:13px;top:130px;left:-10px;width:140px;background:#be2626;background:linear-gradient(to bottom, #be2626, #a92222);padding:0.5em 1.2em;color:white}.dropzone .dz-preview .dz-error-message:after{content:'';position:absolute;top:-6px;left:64px;width:0;height:0;border-left:6px solid transparent;border-right:6px solid transparent;border-bottom:6px solid #be2626}
+    		
     			:host {
     				display: block;
     			}
     		
     			#upload {
-    				border: 1px solid rgb(51, 51, 51);
+    				color: rgb(116, 116, 116);
+    				border: 1px solid rgba(116, 116, 116, 0.2);
     			}
     			
     			#upload .dz-image {
     				border-radius: 0px;
+    				border: 1px solid rgba(116, 116, 116, 0.2);
+    			}
+    			
+    			#upload .dz-error-message {
+    			    display: none;
+    			}
+    			
+    			#upload .dz-remove {
+    				position: absolute;
+    				top: 100%;
+    				left: 0px;
+    				width: 100%;
+    				color: #DB3434;
+    				background: #FBE6E6;
+    				border: 1px solid #DB3434;
+    				font-size: 0.8em;
+    				text-decoration: none;
+    				padding: 2px 4px 3px 4px;
+    				line-height: 0.8em;
+    			}
+    			
+    			#upload .dz-remove:hover {
+    				background: #DB3434;
+    				color: #FBE6E6;
     			}
     		</style>
     		<div id="upload" class="dropzone"></div>
